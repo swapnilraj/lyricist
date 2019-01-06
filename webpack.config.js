5c8147805b16dab1cc91e9c8805e041a9d80bdbd
Swapnil Raj <raj.swapnil99@gmail.com>
2019-01-06 23:41:12 +0530

    Add extension version and bugsnag api key to the environment

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,9 @@ const { join } = require('path');
 const webpack = require('webpack');
 const buildFolder = join(__dirname, 'build');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const pkg = require('./package.json');
+
 module.exports = {
   target: 'web',
   mode: 'development',
@@ -23,9 +26,13 @@ module.exports = {
     filename: '[name].js',
   },
   plugins: [
+    new webpack.EnvironmentPlugin({
+      BUGSNAG_API_KEY: '13895fd55b3e7a9400a21ca642df098e',
+      VERSION: pkg.version,
+    }),
     new CopyWebpackPlugin([
       { from: './icons/', to: `${buildFolder}/icons/` },
-    ])
+    ]),
   ],
   devtool: 'source-map',
   resolve: {
