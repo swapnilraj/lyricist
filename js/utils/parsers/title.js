d94c083b06d14842c5931af734d1b14b4b1e7d0f
Swapnil Raj <raj.swapnil99@gmail.com>
2019-01-09 01:03:37 +0530

    Add boilerplate for try catch around code responsible for parsing the video title

--- js/utils/parsers/title.js
+++ js/utils/parsers/title.js
@@ -1,7 +1,19 @@
+import bugsnag from '@bugsnag/js';
+
+import { bgsngBaseConfig, FAIL_PRIORITIES } from '../bugsnag';
+
 const TITLE_SPLIT_REGEX = /(.*)(\||\/\/\/|-|:)(.*)(\s-\sYouTube)$/;
 const BRACKET_REGEX = /(\(|\[|\{).*(\}|\]|\))/;
 const FEAT_REGEX = /(\sfeat|\sft.).*/i;
 const OFFICIAL_REGEX = /(\sofficial video).*/i;
+
+const bgsngConfig = {
+  ...bgsngBaseConfig,
+  metadata: {
+    video_title: getTitle,
+    priority: FAIL_PRIORITIES.NO_TAG,
+  }
+}
 
 const getTitle = () => document.title;
 
@@ -10,13 +22,19 @@ const getTitle = () => document.title;
  * @returns {metadata} Metadata for the lyrics provider
  */
 const sandTitle = title => {
-  const matches = title
-    .match(TITLE_SPLIT_REGEX)
-    .map(s => s.trim()
-      .replace(BRACKET_REGEX, "")
-      .replace(FEAT_REGEX, "")
-      .replace(OFFICIAL_REGEX, "")
-    );
+  try {
+    const matches = title
+      .match(TITLE_SPLIT_REGEX)
+      .map(s => s.trim()
+        .replace(BRACKET_REGEX, "")
+        .replace(FEAT_REGEX, "")
+        .replace(OFFICIAL_REGEX, "")
+      );
+  } catch (err) {
+    if (err instanceof TypeError) {
+      // notify bugsnag of the error with the metadata decided.
+    }
+  }
   return {
     artist: matches[1],
     title: matches[3],
